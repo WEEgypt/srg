ce15d958ed9de21405d12a412076372ae5b1bc09
Mamdouh Khalaf <100497009+mamdou75alaf@users.noreply.github.com>
2024-09-09 19:17:48 +0300

    Update area-js.js

--- beta/area-js.js
+++ beta/area-js.js
@@ -594,10 +594,11 @@ function Print__1() {
     doc.document.write("<html lang=en><head><title>Area Report</title><style>body {font-family: monospace; display: flex;} div {margin: 0px auto 0px auto;}</style></head><body><div>");
     doc.document.write(document.getElementById("salesreport").value.replace(/  /gi, "&nbsp;&nbsp;").replace(/\n/gi, "<br>"));
     doc.document.write("</div></body></html>");
+	doc.window.print();
     doc.document.close();
 	setTimeout(() => {
 		doc.window.close();
-	}, 1000);
+	}, 2000);
 }
 function Save__1() {
     let x = parseInt(localStorage.getItem("currentArea"));
