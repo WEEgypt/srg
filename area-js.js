47dfc417abad6d4b6eab9e1ead9e433724a0b083
Mamdouh Khalaf <100497009+mamdou75alaf@users.noreply.github.com>
2024-09-09 22:55:10 +0300

    Update area-js.js

--- area-js.js
+++ area-js.js
@@ -597,8 +597,7 @@ function Print__1() {
     doc.document.close();
     doc.window.print();
 
-setTimeout(function () { doc.window.print(); }, 500);
-        doc.window.onfocus = function () { setTimeout(function () { doc.window.close(); }, 500); }
+    doc.window.onfocus = function () { setTimeout(function () { doc.window.close(); }, 500);
 
 
 
