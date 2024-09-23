580c482c7b14f55960106e47d2ac7f62bf133909
Mamdouh Khalaf <100497009+mamdou75alaf@users.noreply.github.com>
2024-09-24 02:29:06 +0300

    Sales Report Generator - PWA _ V1

--- service-worker.js
+++ service-worker.js
@@ -1,44 +1,49 @@
-{
-    if ("serviceWorker" in navigator) {
-        window.addEventListener("load", () => {
-            navigator.serviceWorker
-                .register("service-worker.js")
-                .then((registration) => {
-                    console.log("Service Worker registered with scope:", registration.scope);
-                })
-                .catch((error) => {
-                    console.log("Service Worker registration failed:", error);
-                });
-        });
-    }
-}
+const CACHE_NAME = "srg-v1";
+const ASSETS_TO_CACHE = [
+    "./",
+    "./icon.png",
+    "./icon512_rounded.png",
+    "./icon512_maskable.png",
+    "./index.html",
+    "./store.html",
+    "./agent.html",
+    "./area.html",
+    "./404.html",
+    "./script.js",
+    "./store.js",
+    "./agent.js",
+    "./area.js",
+    "./service-worker.js",
+    "./transformer.js",
+    "./style.css",
+    "./manifest.json",
+];
 self.addEventListener("install", (event) => {
     event.waitUntil(
-        caches.open("SRG-cache-v1").then((cache) => {
-            return cache.addAll([
-                "agent.html",
-                "agent-js.js",
-                "area.html",
-                "area-js.js",
-                "icon.png",
-                "icon512_maskable.png",
-                "icon512_rounded.png",
-                "index.html",
-                "manifest.json",
-                "pulltorefresh.js",
-                "service-worker.js",
-                "store.html",
-                "store-js.js",
-                "style.css",
-                "transformer.js",
-            ]);
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log("Opened cache");
+            return cache.addAll(ASSETS_TO_CACHE);
         })
     );
+    self.skipWaiting();
+});
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(cacheNames.filter((cacheName) => cacheName !== CACHE_NAME).map((cacheName) => caches.delete(cacheName)));
+        })
+    );
+    self.clients.claim();
 });
 self.addEventListener("fetch", (event) => {
     event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
+        caches
+            .match(event.request)
+            .then((response) => {
+                return response || fetch(event.request);
+            })
+            .catch(() => {
+                return caches.match("./404.html");
+            })
     );
 });
