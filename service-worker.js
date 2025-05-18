b5de615ace08499abda0b4f1b02a53842b60e9d4
mamdou75alaf <mamdou7.khalaf@gmail.com>
2025-05-18 17:12:33 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,76 +1,69 @@
-const CACHE_NAME = "srg-v1";
+const CACHE_NAME = "srg-v2";
 const OFFLINE_URL = "./offline.html";
 const NOT_FOUND_URL = "./404.html";
 const ASSETS_TO_CACHE = [
-  "./",
-  "./404.html",
-  "./agent.html",
-  "./agent.js",
-  "./area.html",
-  "./area.js",
-  "./icon512_maskable.png",
-  "./icon512.png",
-  "./index.html",
-  "./main.js",
-  "./manifest.json",
-  "./offline.html",
-  "./service-worker.js",
-  "./store.html",
-  "./store.js",
-  "./style.css",
-  "./transformer.js",
+    "./",
+    "./404.html",
+    "./agent.html",
+    "./agent.js",
+    "./area.html",
+    "./area.js",
+    "./icon512_maskable.png",
+    "./icon512.png",
+    "./index.html",
+    "./main.js",
+    "./manifest.json",
+    "./offline.html",
+    "./service-worker.js",
+    "./store.html",
+    "./store.js",
+    "./style.css",
+    "./transformer.js",
 ];
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS_TO_CACHE);
-    })
-  );
-  self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
+    self.skipWaiting();
 });
 self.addEventListener("fetch", (event) => {
-  const requestURL = new URL(event.request.url);
-  if (
-    requestURL.protocol === "chrome-extension:" ||
-    event.request.method !== "GET"
-  ) {
-    return;
-  }
-  event.respondWith(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.match(event.request).then((cachedResponse) => {
-        const fetchPromise = fetch(event.request)
-          .then((networkResponse) => {
-            if (
-              networkResponse &&
-              networkResponse.status === 200 &&
-              networkResponse.type !== "opaque"
-            ) {
-              cache.put(event.request, networkResponse.clone());
-            }
-            if (networkResponse && networkResponse.status === 404) {
-              return caches.match(NOT_FOUND_URL);
-            }
-            return networkResponse;
-          })
-          .catch(() => {
-            return caches.match(OFFLINE_URL);
-          });
-        return cachedResponse || fetchPromise;
-      });
-    })
-  );
+    const requestURL = new URL(event.request.url);
+    if (requestURL.protocol === "chrome-extension:" || event.request.method !== "GET") {
+        return;
+    }
+    event.respondWith(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.match(event.request).then((cachedResponse) => {
+                const fetchPromise = fetch(event.request)
+                    .then((networkResponse) => {
+                        if (networkResponse && networkResponse.status === 200 && networkResponse.type !== "opaque") {
+                            cache.put(event.request, networkResponse.clone());
+                        }
+                        if (networkResponse && networkResponse.status === 404) {
+                            return caches.match(NOT_FOUND_URL);
+                        }
+                        return networkResponse;
+                    })
+                    .catch(() => {
+                        return caches.match(OFFLINE_URL);
+                    });
+                return cachedResponse || fetchPromise;
+            });
+        })
+    );
 });
 self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
         })
-      );
-    })
-  );
+    );
 });
